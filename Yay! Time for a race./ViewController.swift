2fbd60ca6c72566092cfc1eb4a0f4e5fc7c423f3
Madelin Woods <mwoods2@illinois.edu>
2016-07-23 21:57:21 -0400

    Initial Commit

---
+++ Yay! Time for a race./ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yay! Time for a race.
+//
+//  Created by Madelin Woods on 7/23/16.
+//  Copyright © 2016 Madelin Woods. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
